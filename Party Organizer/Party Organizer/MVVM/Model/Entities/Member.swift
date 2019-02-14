9eda69068961d8e96b373ae89881c4a6027da422
Vladimir Dinic <vladimir88dev@gmail.com>
2019-02-14 22:39:53 +0100

    MVVM  - in progress

--- Party Organizer/Party Organizer/MVVM/Model/Entities/Member.swift
+++ Party Organizer/Party Organizer/MVVM/Model/Entities/Member.swift
@@ -6,8 +6,38 @@
 //  Copyright © 2019 Vladimir Dinic. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import SwiftyJSON
 
 class Member: NSObject {
 
+    private enum Key: String {
+        case id
+        case username
+        case cell
+        case email
+        case gender
+        case photo
+        case aboutMe
+    }
+    
+    var id: Int64
+    var username: String
+    var cell: String
+    var photo: String
+    var email: String
+    var gender: String
+    var aboutMe: String
+    
+    init(data: [String: Any]) {
+        let json = JSON(data)
+        id = json[Key.id.rawValue].int64Value
+        username = json[Key.username.rawValue].stringValue
+        cell = json[Key.cell.rawValue].stringValue
+        photo = json[Key.photo.rawValue].stringValue
+        aboutMe = json[Key.aboutMe.rawValue].stringValue
+        email = json[Key.email.rawValue].stringValue
+        gender = json[Key.gender.rawValue].stringValue
+    }
+    
 }
--- Party Organizer/Pods/Target Support Files/Pods-Party Organizer/Pods-Party Organizer-frameworks.sh
+++ Party Organizer/Pods/Target Support Files/Pods-Party Organizer/Pods-Party Organizer-frameworks.sh
@@ -150,18 +150,22 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AERecord/AERecord.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AERecord/AERecord.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
