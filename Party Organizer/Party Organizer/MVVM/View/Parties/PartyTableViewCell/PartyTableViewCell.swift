d005a0493e62d1c4379560416eee5c318a8790b8
Vladimir Dinic <vladimir88dev@gmail.com>
2019-02-17 13:36:50 +0100

    Adding comments in progress

--- Party Organizer/Party Organizer/MVVM/View/Parties/PartyTableViewCell/PartyTableViewCell.swift
+++ Party Organizer/Party Organizer/MVVM/View/Parties/PartyTableViewCell/PartyTableViewCell.swift
@@ -10,13 +10,26 @@
 
 import UIKit
 
+/// UITableViewCell which is used for showing information about the single party
 class PartyTableViewCell: UITableViewCell {
     
+    // MARK: - Properties
+    
+    /// Party viewModel
     var partyViewModel: PartyViewModel?
     
+    // MARK: - Outlets
+    
+    /// UILabel which shows party name
     @IBOutlet weak var partyNameLabel: UILabel!
+    
+    /// UILabel which shows party start time
     @IBOutlet weak var partyTimeLabel: UILabel!
+    
+    /// UILabel which shows party description
     @IBOutlet weak var partyDescriptionLabel: UILabel!
+    
+    // MARK: - Lifecycle methods
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- Party Organizer/Party Organizer/Utilities/Protocols/TextEditingProtocol.swift
+++ Party Organizer/Party Organizer/Utilities/Protocols/TextEditingProtocol.swift
@@ -8,22 +8,36 @@
 
 import UIKit
 
+
+/// Protocol for managing toolbar above the keyboard for UITextField and UITextView
 @objc protocol TextEditingProtocol {
     
 }
 
 
+/// Private helper protocol for supporting TextEditingProtocol
 @objc fileprivate protocol TextEditingPrivateProtocol {
+    
+    /// Finish editing
     @objc func finishEditing()
+    
+    /// Cancel editing
     @objc func cancelEditing()
 }
 
+
+/// Associated keys which enables us to have properties in extension.
 fileprivate struct AssociatedKeys {
+    
+    /// Associated key for closure / method which is called when user finishes editing the text
     static var onFinishEditing = "onFinishEditing"
 }
 
+// MARK: - TextEditingPrivateProtocol extension with a calculated property (closure for handling text editing finish event)
 extension TextEditingPrivateProtocol {
     
+    
+    /// Calculated property (closure) for handling text editing finish event
     fileprivate var onFinishEditing: voidMethod? {
         get {
             return objc_getAssociatedObject(self, &AssociatedKeys.onFinishEditing) as? voidMethod
@@ -34,6 +48,8 @@ extension TextEditingPrivateProtocol {
     }
 }
 
+
+// MARK: - TextEditingProtocol extension with a method for setting toolbar above the keyboard for handling text editing events
 extension TextEditingProtocol {
     
     func addEndEditingToolbar(title: String? = nil, withCancelButton: Bool = false, onFinishEditing: voidMethod? = nil) {
@@ -56,19 +72,27 @@ extension TextEditingProtocol {
     }
 }
 
+// MARK: - UIView conforms to TextEditingPrivateProtocol, but only in fileprivate scope
 extension UIView: TextEditingPrivateProtocol {
     
-    func finishEditing() {
+    
+    /// Finish editing
+    fileprivate func finishEditing() {
         onFinishEditing?()
         endEditing(true)
         
     }
     
-    func cancelEditing() {
+    /// Cancel editing
+    fileprivate func cancelEditing() {
         endEditing(true)
     }
 }
 
+
+// MARK: - UITextField conforms to TextEditingProtocol
 extension UITextField: TextEditingProtocol { }
+
+// MARK: - UITextView conforms to TextEditingProtocol
 extension UITextView: TextEditingProtocol { }
 
